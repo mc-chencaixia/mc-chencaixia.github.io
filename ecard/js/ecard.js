b46e9ba518ad560fd0c1e57314c5be7a5af61ae2
mc-chencaixia <chencx@mchz.com.vn>
2016-04-15 17:01:30 +0800

    ecard

--- ecard/js/ecard.js
+++ ecard/js/ecard.js
@@ -15,9 +15,9 @@ $(function() {
 	var btnRst = $('#J_btnRst');
 	var posMap = {
 		'name':{x: 0, y: 30, fontsize:'24px', color:'#5f5d5d'},
-		'title':{x: 85, y: 25},
-		'phone':{x: 220, y: 28, fontsize:'18px', color:'#5f5d5d'},
-		'telephone':{x: 40, y: 95},
+		'title':{x: 85, y: 29},
+		'phone':{x: 260, y: 29, fontsize:'12px', color:'#5f5d5d'},
+		'telephone':{x: 80, y: 95},
 		'email':{x: 40, y: 115},
 		'net':{x: 40, y: 139},
 		'website':{x: 240, y: 114},
