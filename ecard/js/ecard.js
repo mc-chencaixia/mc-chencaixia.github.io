8b1cf82c94b8ab53443a428420cd0657eb317772
mc-chencaixia <chencx@mchz.com.vn>
2016-04-15 16:28:20 +0800

    ecard

--- ecard/js/ecard.js
+++ ecard/js/ecard.js
@@ -12,6 +12,7 @@ $(function() {
 	var iptObjs = $('input');
 	var formObj = $('form');
 	var btnOk = $('#J_btnOk');
+	var btnRst = $('#J_btnRst');
 	var posMap = {
 		'name':{x: 10, y: 30, fontsize:'24px', color:'#5f5d5d'},
 		'title':{x: 85, y: 25},
@@ -23,7 +24,16 @@ $(function() {
 		'blog':{x: 240, y: 93},
 		'address':{x: 40, y: 164},
 	};
-	var info = {'net':'杭州、广州、南京、武汉、宁波'};
+	var defaultInfo = {
+			'net':'杭州、广州、南京、武汉、宁波',
+			'website':'www.mchz.com.cn',
+			'name':'姓名',
+			'title':'职位信息',
+		};
+	var info = {
+			'net':'杭州、广州、南京、武汉、宁波',
+			'website':'www.mchz.com.cn',
+		};
 	
 	// 画布背景重绘
 	var drawBg = function(){
@@ -33,19 +43,20 @@ $(function() {
 	var drawText = function( text, pos ){
 		//console.log(text);
 		//console.log(pos);
+		if(!text || !pos){
+			return false;
+		}
 		var fs = pos.fontsize || '12px';
 		var cl = pos.color || '#898989';
 		var font = pos.font || '微软雅黑';
 		ctx.font = fs + ' ' + font;
 		ctx.fillStyle = cl;
-		ctx.fillText(text, pos.x, pos.y);
-		
-		
+		ctx.fillText(text, pos.x, pos.y);		
 	};
 	// 获取信息
 	var getInfo = function(){
 		$.each( iptObjs, function(k,v){
-			info[v.id] = v.value;
+			info[v.id] = v.value || defaultInfo[v.id];
 		});
 	}
 	
@@ -62,6 +73,12 @@ $(function() {
 		}
 	}
 	
+	var renderCard = function(){
+		drawBg();
+		getInfo();
+		drawInfo();
+	}
+	
 	formObj.on('blur','input',function(ev){
 		var it = ev.currentTarget;
 		info[it.id] = it.value;
@@ -74,11 +91,15 @@ $(function() {
 		console.log('生成图片');
 	});
 	
+	btnRst.on('click',function(){
+		formObj[0].reset();
+		renderCard();
+		
+	})
+	
 	// 初始化
 	var init = function(){
-		drawBg();
-		getInfo();
-		drawInfo();
+		renderCard();
 	}
 	
 	init();
